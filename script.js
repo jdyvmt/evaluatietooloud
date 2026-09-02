35b48d2ff5f20b760ad25b9232452d36e4b4cb1c
jdyvmt <jordy.vermote@gmail.com>
2026-09-02 21:56:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1599,3 +1599,7 @@ function moveLevel(criterionIdx, levelIdx, direction) {
   
   renderCriteriaEditor();
 }
+alert(
+  "html2pdf = " + typeof html2pdf +
+  "\nhtml2canvas = " + typeof html2canvas
+);
